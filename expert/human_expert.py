43cdbf699756345dcf668e82a5abf3477682a4e7
Maria <mramire8@hawk.iit.edu>
2015-03-24 15:00:08 -0500

    update human expert

--- expert/human_expert.py
+++ expert/human_expert.py
@@ -1,6 +1,7 @@
 __author__ = 'maru'
 
 from expert.base import BaseExpert
+import os
 
 class HumanExpert(BaseExpert):
 
@@ -23,13 +24,15 @@ class HumanExpert(BaseExpert):
             print
             print ("-"*40)
             print
-            print '\033[94m' + textwrap.fill(print_text, width=70) +'\033[0m'
+            # print '\033[94m' + textwrap.fill(print_text, width=70) +'\033[0m'
+            print self.print_text(textwrap.fill(print_text, width=70),'blue')
             t0 = time.time()
             valid = False
             answer = -1
             while not valid:
                 print
-                answer = raw_input('\033[92m'+self.prompt+'\033[0m')
+                # answer = raw_input('\033[92m'+self.prompt+'\033[0m')
+                answer = raw_input(self.print_text(self.prompt, 'green'))
                 try:
                     answer = int(answer)
                     if answer not in range(0, self.num_classes):
@@ -44,15 +47,28 @@ class HumanExpert(BaseExpert):
                             self.pauses_left -= 1
                             print "%s pauses left" % self.pauses_left
                             print "\n*** PAUSE ***\n"
-                            pause = raw_input('\033[92mpress <return> to continue...\033[0m')
+                            # pause = raw_input('\033[92mpress <return> to continue...\033[0m')
+                            raw_input(self.print_text('press <return> to continue...', 'green'))
                             print ("-"*40)
                             print
-                            print '\033[94m' + textwrap.fill(print_text, width=70) +'\033[0m'
+                            # print '\033[94m' + textwrap.fill(print_text, width=70) +'\033[0m'
+                            print self.print_text(textwrap.fill(print_text, width=70),'blue')
                             t0 = time.time()
                             self.paused = True
             self.elapsed_time = time.time() - t0
             times.append(self.elapsed_time)
         return labels
+
+    def print_text(self, text, color):
+        if "nt" in os.name:
+            return text
+        else:
+            if color == 'blue':
+                return '\033[94m' + text +'\033[0m'
+            elif color == 'green':
+                return '\033[92m'+text+'\033[0m'
+            else:
+                return text
 
     def fit(self, data, y=None):
         pass
